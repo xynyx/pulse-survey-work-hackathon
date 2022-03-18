ead09745b484432508f9aee76e7e5033310c801a
xynyx <matt.taylr22@gmail.com>
2022-03-18 11:19:29 -0400

    Fix file name; error handling

--- controllers/createAnswers.js
+++ controllers/createAnswers.js
@@ -2,15 +2,17 @@ import { createAnswers } from "../services/answerService.js";
 import { updateCompletionData } from "../services/completionService.js";
 
 export default async ({ request, response }) => {
-  const { answers, surveyId, userId } = await request.body().value;
+  const requestBody = await request.body().value;
+
+  if (requestBody?.answers?.length === 0 || !requestBody?.surveyId || !requestBody?.userId) {
+    response.status = 422;
+    response.body = { msg: "Incorrect answer data. Answers, surveyId and userId are required" };
+    return;
+  }
 
   console.log(await request.body({ type: "json" }).value);
 
-  if (answers.length === 0 || !surveyId || !userId) {
-    response.status = 422;
-    response.body = { msg: "Incorrect answer data. Answers are required" };
-    return;
-  }
+  const { answers, surveyId, userId } = requestBody;
 
   await createAnswers(answers);
   await updateCompletionData(userId, surveyId);
