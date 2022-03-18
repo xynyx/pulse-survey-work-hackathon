9a2f2387f9b6473c1d04f670f955e924c9c88896
Jack Zavarella <jack@gobridgit.com>
2022-03-18 11:41:47 -0400

    disables cors

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import { Application } from "https://deno.land/x/oak@v8.0.0/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import { APP_HOST, APP_PORT } from "./config.js";
 import router from "./routes.js";
 import client from "./db/database.js";
 // import _404 from "./controllers/404.js";
 const app = new Application();
 
+app.use(
+  oakCors({
+    origin: "*"
+  }),
+);
 app.use(router.routes());
 app.use(router.allowedMethods());
 await client.connect();
